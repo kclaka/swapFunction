260515ae58339d42e357d5f6dbbce6d7c5b678b8
kclaka <kennyigbechi@gmail.com>
2020-03-20 01:36:06 -0700

    Add project files.

---
+++ swap/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace swap
+{
+    class Program
+    {
+        static void Swap(ref int x, ref int y)
+        {
+            
+            int temp = x;
+            x = y;
+            y = temp;
+
+            
+        }
+        static void Main(string[] args)
+        {
+            int x = 4;
+            int y = 6;
+
+            Swap(ref x, ref y);
+
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
